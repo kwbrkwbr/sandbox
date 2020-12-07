c04b188f22f438c7e84c55dd4e2a2d48a2a344ae
kwbrkwbr <t.kuwabara19820209@gmail.com>
2020-12-07 16:04:36 +0900

    cryptのエンドポイントをはやす (#7)
    Co-authored-by: takashi kuwabara <t.kuwabara@axelentermedia.jp>

--- main.go
+++ main.go
@@ -11,6 +11,10 @@ func main() {
 	e := echo.New()
 	e.GET("/", handler.Hello)
 	e.POST("/mail", handler.Mail)
+	e.POST("/crypt/cfb", handler.CryptCFB)
+	e.POST("/decrypt/cfb", handler.DecryptCFB)
+	e.POST("/crypt/cbc", handler.CryptCBC)
+	e.POST("/decrypt/cbc", handler.DecryptCBC)
 
 	port := os.Getenv("PORT")
 	if port == "" {
